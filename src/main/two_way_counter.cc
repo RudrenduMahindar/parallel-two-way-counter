015194fcc02dcb129f42def0c970498d76dedff6
RudrenduMahindar <mahindar@usc.edu>
2021-01-20 13:41:22 -0800

    adding_locks

--- src/main/two_way_counter.cc
+++ src/main/two_way_counter.cc
@@ -9,18 +9,40 @@
 #include "src/lib/utility.h"
 using namespace std;
 
+bool line_reading_ends = false;
+mutex m;
 queue<string> inp_queue;
 queue<string> adder_queue1;
 queue<string> adder_queue2;
 
 void move_input_to_adder1()
 {
-    cout<<"adder1"<<"\n";
+    //cout<<"adder1"<<"\n";
+    //lock_guard<mutex> guard(m); //calling contructor starts critical section, locks mutex
+    while(1)
+    {
+        m.lock();
+        
+        if(!inp_queue.empty())
+        {
+            string line = inp_queue.front();
+            inp_queue.pop();
+            adder_queue1.push(line);
+            cout<<"t2 added line to adder_queue1\n";
+        }
+        if((inp_queue.empty()) && (line_reading_ends==true))
+        {
+            cout<<"t2 exits\n";
+            m.unlock();
+            break;
+        }
+        m.unlock();
+    }
 }
 
 void move_input_to_adder2()
 {
-    cout<<"adder2"<<"\n";
+    //cout<<"adder2"<<"\n";
 }
 
 void read_input_lines()
@@ -31,9 +53,17 @@ void read_input_lines()
         string read_line;
         while(getline(file_obj, read_line)) //read data from file object and put it into string.
         {
+            //critical section
+            m.lock();
+            cout<<"t1 adds line"<<"\n";
             inp_queue.push(read_line);
-            cout << read_line << "\n"; //print the data of the string
+            //cout << read_line << "\n"; //print the data of the string
+            m.unlock();
         }
+        m.lock();
+        line_reading_ends = true;
+        cout<<"t1 exits"<<"\n";
+        m.unlock();
         file_obj.close();
     }
 }
