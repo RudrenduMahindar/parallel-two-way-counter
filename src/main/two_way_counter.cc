85f2aa6dea18499868b33b0d0557e1e8a15b8bdf
RudrenduMahindar <mahindar@usc.edu>
2021-01-21 02:16:24 -0800

    multiple_threads

--- src/main/two_way_counter.cc
+++ src/main/two_way_counter.cc
@@ -11,18 +11,22 @@ using namespace std;
 
 bool line_reading_ends = false;
 mutex m;
+condition_variable cv;
+bool line_inp_ready = false;
+
 queue<string> inp_queue;
 queue<string> adder_queue1;
 queue<string> adder_queue2;
 
 void move_input_to_adder1()
 {
-    //cout<<"adder1"<<"\n";
-    //lock_guard<mutex> guard(m); //calling contructor starts critical section, locks mutex
     while(1)
     {
-        m.lock();
-        
+        unique_lock<mutex> u1(m); //calling contructor starts critical section, locks mutex
+        //thread wakes up after line input is available or when no more lines to read (otherwise thread may get stuck when line input adder exits without any notification) 
+        cv.wait(u1, [](){return (line_inp_ready || line_reading_ends);});//wait for notification, then evaluate guard condition
+        line_inp_ready = false;//reset this flag after taking out each line from input queue
+
         if(!inp_queue.empty())
         {
             string line = inp_queue.front();
@@ -30,19 +34,37 @@ void move_input_to_adder1()
             adder_queue1.push(line);
             cout<<"t2 added line to adder_queue1\n";
         }
-        if((inp_queue.empty()) && (line_reading_ends==true))
+
+        if((inp_queue.empty()) && (line_reading_ends == true))
         {
             cout<<"t2 exits\n";
-            m.unlock();
             break;
         }
-        m.unlock();
     }
 }
 
 void move_input_to_adder2()
 {
-    //cout<<"adder2"<<"\n";
+    while(1)
+    {
+        unique_lock<mutex> u1(m);
+        cv.wait(u1, []() { return (line_inp_ready || line_reading_ends); }); //wait for notification
+        line_inp_ready = false;
+
+        if(!inp_queue.empty())
+        {
+            string line = inp_queue.front();
+            inp_queue.pop();
+            adder_queue2.push(line);
+            cout<<"t3 added line to adder_queue2\n";
+        }
+
+        if((inp_queue.empty()) && (line_reading_ends == true))
+        {
+            cout<<"t3 exits\n";
+            break;
+        }
+    }
 }
 
 void read_input_lines()
@@ -56,12 +78,15 @@ void read_input_lines()
             //critical section
             m.lock();
             cout<<"t1 adds line"<<"\n";
-            inp_queue.push(read_line);
+            inp_queue.push(read_line); //shared queue
+            line_inp_ready = true;
+            cv.notify_all();
             //cout << read_line << "\n"; //print the data of the string
             m.unlock();
         }
         m.lock();
         line_reading_ends = true;
+        cv.notify_all(); //last notification to wake up threads from cv queue
         cout<<"t1 exits"<<"\n";
         m.unlock();
         file_obj.close();
